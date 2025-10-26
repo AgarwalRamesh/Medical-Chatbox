6fb9c0b11cf5cfc3ba0b8245918723974ad00926
Agarwal Ramesh <rameshagar01@gmail.com>
2025-10-26 22:42:43 +0530

    Folder Structure Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Projects',
+    version = '0.0.0',
+    author = 'Agarwal Ramesh',
+    author_email = 'rameshagar01@gmail.com',
+    packages = find_packages(),
+    install_required = []
+)
